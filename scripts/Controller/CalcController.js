f0f853ca7c5c633d05526275ac0b54b79a08088c
Jonathanmoreiraa <jonathanf.fmoreira@gmail.com>
2021-03-26 08:43:26 -0300

    Primeira parte do projeto

---
+++ scripts/Controller/CalcController.js
@@ -0,0 +1,125 @@
+class CalcController{
+    constructor(){
+        this._operation = [];
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate;
+        this.initialize();
+        this.initButtonEvents();
+    
+    }
+    initialize(){
+        this.setdisplayDateTime()
+        setInterval(()=>{
+            this.setdisplayDateTime()
+        }, 1000); 
+        
+    }
+    addEventListenerAll(element,events, fn){
+        events.split(' ').forEach(event=>{
+            element.addEventListener(event, fn, false)
+        })
+    }
+    clearAll(){
+        this._operation = [];
+    }
+    clearEntry(){
+        this._operation.pop()
+    }
+    addOperation(value){
+        this._operation.push(value)
+        console.log(this._operation);
+    }
+    setError(){
+        this.displayCalc = "Error";
+    }
+    execBtn(value){
+        switch (value){
+            case 'ac':
+                this.clearAll()
+                break;
+            case 'ce':
+                this.clearEntry()
+                break;
+            case 'soma':
+                break;
+            case 'subtracao':
+                break;
+            case 'divisao':
+                break;
+            case 'multiplicacao':
+                break;
+            case 'porcento':
+                break;
+            case 'igual':
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+        }
+    }
+    initButtonEvents(){
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+        buttons.forEach((btn, index)=>{ //colocar parenteses a partir de dois elmentos
+            this.addEventListenerAll(btn,'click drag', e=>{
+                let textBtn = btn.className.baseVal.replace("btn-","")
+                this.execBtn(textBtn);
+            });
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=>{
+                btn.style.cursor = "pointer"
+            })
+        })
+    }
+
+
+
+    setdisplayDateTime(){
+        /* Para adicionar nome pra data (17 de março de 2018)
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day: "2-digit", //traz o dia em dois digitos
+            month: "long", //traz o mês escrito por extenso
+            year: "numeric", //4 digitos
+        })*/
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale)
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale)
+    }
+    get displayDate(){
+        return this._dateEl.innerHTML
+    }
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+    get displayTime(){
+        return this._timeEl.innerHTML
+    }
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+    get displayCalc(){
+        return this._displayCalcEl.innerHTML;
+    }
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }
+    get currentDate(){
+        return new Date();
+    }
+    set currentDate(date){
+        this._currentDate = date;
+    }
+}
